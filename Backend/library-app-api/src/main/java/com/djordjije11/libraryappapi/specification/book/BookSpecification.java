1c583bf3779764d8cc86ea21b796d94cf5ac020d
djordjije11 <djordjijeradovic24@gmail.com>
2023-07-26 17:56:22 +0200

    Added searching by publisher and authors while searching for books

--- Backend/library-app-api/src/main/java/com/djordjije11/libraryappapi/specification/book/BookSpecification.java
+++ Backend/library-app-api/src/main/java/com/djordjije11/libraryappapi/specification/book/BookSpecification.java
@@ -2,13 +2,12 @@ package com.djordjije11.libraryappapi.specification.book;
 
 import com.djordjije11.libraryappapi.helper.criteriabuilder.CriteriaBuilderHelper;
 import com.djordjije11.libraryappapi.helper.string.util.StringExt;
-import com.djordjije11.libraryappapi.model.Book;
-import com.djordjije11.libraryappapi.model.Book_;
+import com.djordjije11.libraryappapi.model.*;
+import jakarta.persistence.criteria.Join;
 import org.springframework.data.jpa.domain.Specification;
 
 public final class BookSpecification {
     private BookSpecification() {
-
     }
 
     public static Specification<Book> bySearch(String search) {
@@ -16,7 +15,13 @@ public final class BookSpecification {
             if (StringExt.isNullOrBlank(search)) {
                 return CriteriaBuilderHelper.alwaysTruePredicate(criteriaBuilder);
             }
-            return criteriaBuilder.like(root.get(Book_.TITLE), CriteriaBuilderHelper.containsAsSqlLike(search));
+            Join<Book, Author> bookAuthorJoin = root.join(Book_.AUTHORS);
+            return criteriaBuilder.or(
+                    criteriaBuilder.like(root.get(Book_.TITLE), CriteriaBuilderHelper.containsAsSqlLike(search)),
+                    criteriaBuilder.like(root.get(Book_.PUBLISHER).get(Publisher_.NAME), CriteriaBuilderHelper.containsAsSqlLike(search)),
+                    criteriaBuilder.like(bookAuthorJoin.get(Author_.FIRSTNAME), CriteriaBuilderHelper.containsAsSqlLike(search)),
+                    criteriaBuilder.like(bookAuthorJoin.get(Author_.LASTNAME), CriteriaBuilderHelper.containsAsSqlLike(search))
+            );
         };
     }
 }
