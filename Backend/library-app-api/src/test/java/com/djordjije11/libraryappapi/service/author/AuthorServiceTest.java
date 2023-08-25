df8321fc8ba7eb5e235e0ef8190689d518ec9802
djordjije11 <djordjijeradovic24@gmail.com>
2023-08-25 11:28:01 +0200

    Added more tests for AuthorService

--- Backend/library-app-api/src/test/java/com/djordjije11/libraryappapi/service/author/AuthorServiceTest.java
+++ Backend/library-app-api/src/test/java/com/djordjije11/libraryappapi/service/author/AuthorServiceTest.java
@@ -2,7 +2,6 @@ package com.djordjije11.libraryappapi.service.author;
 
 import com.djordjije11.libraryappapi.model.Author;
 import com.djordjije11.libraryappapi.repository.AuthorRepository;
-import com.djordjije11.libraryappapi.specification.author.AuthorSpecification;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
@@ -26,13 +25,13 @@ public abstract class AuthorServiceTest {
     protected AuthorService authorService;
 
     @Test
-    public void get(){
-        var authors = new ArrayList<Author>(){{
-            add(new Author("A Firstname", "Lastname 3", "Biography 1"));
-            add(new Author("Firstname", "Lastname 2", "Biography 2"));
-            add(new Author("C Firstname", "Lastname 3", "Biography 3"));
-            add(new Author("Firstname", "Lastname 4", "Biography 4"));
-            add(new Author("B Firstname", "Lastname 3", "Biography 5"));
+    public void get_search_by_biography() {
+        var authors = new ArrayList<Author>() {{
+            add(new Author("A-Firstname", "Lastname-3", "Biography 1"));
+            add(new Author("Firstname", "Lastname-2", "Biography 2"));
+            add(new Author("C-Firstname", "Lastname-3", "Biography 3"));
+            add(new Author("Firstname", "Lastname-4", "Biography 4"));
+            add(new Author("B-Firstname", "Lastname-3", "Biography 5"));
         }};
         authorRepository.saveAll(authors);
         Page<Author> authorPage = authorService.get("3", PageRequest.of(0, 2, Sort.by(Sort.Direction.ASC, "firstname")));
@@ -43,5 +42,53 @@ public abstract class AuthorServiceTest {
         assertEquals(2, dbAuthors.size());
         assertEquals("Biography 1", dbAuthors.get(0).getBiography());
         assertEquals("Biography 5", dbAuthors.get(1).getBiography());
+    }
+
+    @Test
+    public void get_search_by_biography_when_there_is_no_searched_Author() {
+        var authors = new ArrayList<Author>() {{
+            add(new Author("A-Firstname", "Lastname-3", "Biography 1"));
+            add(new Author("Firstname", "Lastname-2", "Biography 2"));
+            add(new Author("C-Firstname", "Lastname-3", "Biography 3"));
+            add(new Author("Firstname", "Lastname-4", "Biography 4"));
+            add(new Author("B-Firstname", "Lastname-3", "Biography 5"));
+        }};
+        authorRepository.saveAll(authors);
+        Page<Author> authorPage = authorService.get("Biography 999", PageRequest.of(0, 2, Sort.unsorted()));
+        var dbAuthors = authorPage.toList();
+
+        assertNotNull(authorPage);
+        assertNotNull(dbAuthors);
+        assertEquals(0, dbAuthors.size());
+    }
+
+    @Test
+    public void get_search_by_firstname() {
+        var authors = new ArrayList<Author>() {{
+            add(new Author("A-Firstname", "Lastname-3", "Biography 1"));
+            add(new Author("Firstname", "Lastname-2", "Biography 2"));
+            add(new Author("C-Firstname-2", "Lastname-3", "Biography 3"));
+            add(new Author("C-Firstname-1", "Lastname-4", "Biography 4"));
+            add(new Author("B-Firstname", "Lastname-3", "Biography 5"));
+        }};
+        authorRepository.saveAll(authors);
+        Page<Author> authorPage = authorService.get("C-First", PageRequest.of(0, 5, Sort.by(Sort.Direction.DESC, "lastname")));
+        var dbAuthors = authorPage.toList();
+
+        assertNotNull(authorPage);
+        assertNotNull(dbAuthors);
+        assertEquals(2, dbAuthors.size());
+        assertEquals("Lastname-4", dbAuthors.get(0).getLastname());
+        assertEquals("Lastname-3", dbAuthors.get(1).getLastname());
+    }
+
+    @Test
+    public void get_when_there_is_no_Authors_in_the_database() {
+        Page<Author> authorPage = authorService.get("", PageRequest.of(0, 10, Sort.unsorted()));
+        var dbAuthors = authorPage.toList();
+
+        assertNotNull(authorPage);
+        assertNotNull(dbAuthors);
+        assertEquals(0, dbAuthors.size());
     }
 }
